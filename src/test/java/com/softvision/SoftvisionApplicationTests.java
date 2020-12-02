0cc81eb51e2dca3a67736583e05ee341103bce82
Abdulrasheed Mustapha <abdulrasheedmustapha@SVUSLP00213.local>
2020-12-01 21:49:09 -0600

    sample

---
+++ src/test/java/com/softvision/SoftvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
